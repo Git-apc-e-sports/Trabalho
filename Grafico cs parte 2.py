343bfaafd9c82bb2e3a8afd16b8fd5a680c25c98
fsousac <211061707@aluno.unb.br>
2022-04-17 21:05:39 -0300

    mudanças no grafico cs2

--- Grafico cs parte 2.py
+++ Grafico cs parte 2.py
@@ -1,52 +1,95 @@
-# Grafico CS
+#Grafico CS
+
+
+
+
 
 
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
+import numpy as np
 
-df = pd.read_excel('Tabela_dinheiro_por_ano.xlsx')
 
+
+
+a= []
+df=pd.read_excel(r'Data base definitivo.xlsx')
+df.dropna(inplace=True)
 df_array = df.values
+lista_array = df_array.tolist()
 
-anos_px = []
-for coluna in df_array:
-    try:
-        anos_px.append(coluna[0])
-    except:
-        continue
-anos_px_total = 0
-anos_px_filtragem = []
-total = []
-premio_total = []
-for coluna in df_array:
-    try:
-        premio_total.append(coluna[1])
-    except:
-        continue
+
+#print(type(lista_array[0]))
+##print(df.shape)
+
+
+
+ganhos_cs = []
+data = []
+contador = 0
+n = 1
+datas = []
+raw = [lista_array[i:i+n] for i in range(0, len(lista_array), n)]
+for i in range(len(raw)):
+    datas = raw.pop()
+    print(datas)
+#ganhos_cs = colunas [20]
+#print (datas)
+#print (ganhos_cs)    
+    
+
+
+
+#print(data)
+contador = 0
+for celula in df:
+    contador += 1
+    if contador == 21:
+        ganhos_cs.append(celula)
+ganhos_cs = df["ganhos_cs"].tolist()
+data = df["data"].tolist()
+
+
+
+
+
 
 fig = go.Figure()
 
-fig.update_yaxes(title='Prêmios', title_font_color='black',
-                 title_font_family="Overpass",
-                 title_font_size=20, showgrid=True)
 
-fig.update_xaxes(title='Ano', title_font_color='black',
-                 title_font_family="Overpass",
-                 title_font_size=20, showgrid=False)
 
-fig.update_layout(title='Prêmios de campeonatos de CS:GO', title_pad_r=10000,
-                  plot_bgcolor='#edbb05',
-                  paper_bgcolor='#bd9c24', title_xanchor='left',
-                  title_yanchor='top', title_xref='paper', title_yref='paper', title_x=0.23,
-                  title_font_family="Gravitas One", title_font_size=30,
-                  font=dict(color='black'))
 
-fig.update_layout(showlegend=True)
 
-fig.add_trace(go.Scatter(x=anos_px, y=premio_total, name='',
-                         line=dict(color='black', width=3,
-                                   )))
+fig.update_yaxes(title = 'Prêmios', title_font_color = 'black',
+                 title_font_family = "Overpass",
+title_font_size = 20, showgrid= True)
+
+
+
+fig.update_xaxes(title = 'Ano', title_font_color = 'black',
+                 title_font_family = "Overpass",
+title_font_size = 20, showgrid=False)
+
+
+
+fig.update_layout(title = 'Prêmios de campeonatos de CS:GO', title_pad_r=10000,
+                  plot_bgcolor= '#edbb05' ,
+ paper_bgcolor='#bd9c24', title_xanchor= 'left',
+ title_yanchor= 'top', title_xref= 'paper', title_yref= 'paper', title_x= 0.23,
+ title_font_family = "Gravitas One", title_font_size = 30,
+ font=dict(color='black'))
+
+
+
+fig.update_layout(showlegend= True)
+
+
+
+
+fig.add_trace(go.Scatter(x=anos_px, y=premio_total, name =  '',
+                         line = dict(color='black', width=3,
+)))
 fig.add_trace(
     go.Scatter(
         mode='markers',
@@ -63,4 +106,5 @@ fig.add_trace(
     )
 )
 
-fig.show()
+
+#fig.show()
