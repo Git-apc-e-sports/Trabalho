47b6f83a38f1b1f0aa33e076fcd71437dbf0efeb
fsousac <211061707@aluno.unb.br>
2022-04-21 14:49:59 -0300

    implantação do grafico de cs no dash

--- Grafico cs parte 2.py
+++ Grafico cs parte 2.py
@@ -2,102 +2,93 @@
 
 
 
-
-
-
-import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
-import numpy as np
+from dash import Dash, html, dcc
 
 
+#implantação do dash
+app = Dash(__name__)
 
 
-a= []
+#leitura dos dados da data base
 df=pd.read_excel(r'Data base definitivo.xlsx')
-df.dropna(inplace=True)
-df_array = df.values
-lista_array = df_array.tolist()
-
-
-#print(type(lista_array[0]))
-##print(df.shape)
+df.dropna(inplace=True)                                     #retirada de dados nulos
+df_array = df.values                                        #definição da array com os valores da db
 
 
 
+#criação de listas vazias para receber os valores de cada coluna
 ganhos_cs = []
-data = []
-contador = 0
-n = 1
 datas = []
-raw = [lista_array[i:i+n] for i in range(0, len(lista_array), n)]
-for i in range(len(raw)):
-    datas = raw.pop()
-    print(datas)
-#ganhos_cs = colunas [20]
-#print (datas)
-#print (ganhos_cs)    
-    
+index = 0
+
+#leitura dos arrays transformando em listas de cada coluna
+for coluna in df_array:
+    datas.append(coluna[0])
+
+for coluna in df_array:
+    ganhos_cs.append(coluna[20])
+
+
+datas = datas[27:]
+ganhos_cs = ganhos_cs[27:]
+#confecção de uma lista geral com formato ano/valor
+lista_all = []
+index = 0
+for a in datas:
+    for c in ganhos_cs:
+        lista_all.append(a)
+        lista_all.append(ganhos_cs[index])
+        index += 1
+        break
 
 
 
-#print(data)
-contador = 0
-for celula in df:
-    contador += 1
-    if contador == 21:
-        ganhos_cs.append(celula)
-ganhos_cs = df["ganhos_cs"].tolist()
-data = df["data"].tolist()
-
-
-
-
-
-
+#criacao de grafico
 fig = go.Figure()
 
 
-
-
-
+#-----------------------------------------organização do eixo y do grafico----------------------------------------
 fig.update_yaxes(title = 'Prêmios', title_font_color = 'black',
                  title_font_family = "Overpass",
 title_font_size = 20, showgrid= True)
 
 
-
+#------------------------------------organização do eixo x do grafico--------------------------------------
 fig.update_xaxes(title = 'Ano', title_font_color = 'black',
                  title_font_family = "Overpass",
 title_font_size = 20, showgrid=False)
 
 
-
-fig.update_layout(title = 'Prêmios de campeonatos de CS:GO', title_pad_r=10000,
-                  plot_bgcolor= '#edbb05' ,
- paper_bgcolor='#bd9c24', title_xanchor= 'left',
- title_yanchor= 'top', title_xref= 'paper', title_yref= 'paper', title_x= 0.23,
+#------------------------------------organização do titulo do grafico-----------------------------------------
+fig.update_layout(title = 'Prêmios de campeonatos de CS:GO',
+                  plot_bgcolor= 'black' ,
+ paper_bgcolor='black', 
+ title_yanchor= 'top', title_xref= 'paper', title_yref= 'paper',
  title_font_family = "Gravitas One", title_font_size = 30,
- font=dict(color='black'))
+ font=dict(color='white'))
 
 
-
+# ativação da legenda para a ativação e desativação do grafico
 fig.update_layout(showlegend= True)
 
 
+# criação da linha para o grafico de linhas
 
-
-fig.add_trace(go.Scatter(x=anos_px, y=premio_total, name =  '',
-                         line = dict(color='black', width=3,
+fig.add_trace(go.Scatter(x=datas, y=ganhos_cs, name =  '',
+                         line = dict(color='lightskyblue', width=3,
 )))
+
+
 fig.add_trace(
     go.Scatter(
         mode='markers',
-        x=anos_px,
-        y=premio_total,
+        x=datas,
+        y=ganhos_cs,
         marker=dict(
-            color='white',
-            size=10,
+            color='aquamarine',
+            size=5,
 
         ),
 
@@ -105,6 +96,17 @@ fig.add_trace(
 
     )
 )
+#--------------------------------------- implementação de funçoes do dash----------------------------
 
+app.layout = html.Div(children=[
+    html.H1(children='Gráfico Prêmios por mês em campeonatos de Counter Strike'),
+    html.Div(children='''
+        Gráfico relacionando meses com os Prêmios cumulativos de cada ano, de 2012 a 2022'''),
+        dcc.Graph(id='grafico cs principal', figure=fig)
 
-#fig.show()
+])
+
+if __name__ == '__main__' :
+    app.run_server(debug=True)
+
+fig.show()
